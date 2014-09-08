39052abd221dae843fbdad1f57144be48c3bbc53
Jérôme Macias <jerome.macias@gmail.com>
2014-09-08 16:41:31 +0200

    Implement directory navigation@

--- gotree.go
+++ gotree.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/nsf/termbox-go"
 	"io/ioutil"
-	//"os"
-	//"path/filepath"
 	"strings"
 )
 
 var termboxLine int
-var folderLevelMinned int
 var rootPath string
+var currentPath string
+
+var currentLine int
+var files []File
+
+type File struct {
+	isDir 	bool
+	name   	string
+}
 
 func init() {
 	termboxLine = 1
-	folderLevelMinned = 0
+	currentLine = 0
+	rootPath = "."
 }
 
 func main() {
+	flag.Parse()
+	if userpath := flag.Arg(0); userpath != "" {
+		rootPath = userpath
+	}
+	currentPath = rootPath
+
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
@@ -30,21 +42,27 @@ func main() {
 
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-	flag.Parse()
-	rootPath := "./"
-	if userpath := flag.Arg(0); userpath != "" {
-		rootPath = userpath
-	}
 	//rootPathTest, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	displayFirstLine(rootPath)
-	displayDir(rootPath, "")
+	displayDir(rootPath)
+
+	termbox.Flush()
+
 loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
 			switch ev.Key {
-			case termbox.KeyEsc:
-				break loop
+				case termbox.KeyEsc:
+					break loop
+				case termbox.KeyArrowDown:
+					changeSelect("down")
+				case termbox.KeyArrowUp:
+					changeSelect("up")
+				case termbox.KeyArrowRight:
+					enterDir()
+				case termbox.KeyArrowLeft:
+					leaveDir()
 			}
 		}
 	}
@@ -63,39 +81,32 @@ func displayFirstLine(rootPath string) {
 	}
 }
 
-func displayDir(path string, previousIndent string) {
+func displayDir(path string) {
+	dirFiles, _ := ioutil.ReadDir(path)
+	first := true
+	i := 0
 
-	files, _ := ioutil.ReadDir(path)
-	nbElements := len(files)
-	indent := "├── "
-	nextIndent := "│       "
+	for _, f := range dirFiles {
+		if f.IsDir() && strings.HasPrefix(f.Name(), ".") {
+			continue
+		}
 
-	for y, f := range files {
-		if y == nbElements-1 {
-			indent = "└── "
-			nextIndent = "       "
+		if 0 < i {
+			first = false
 		}
-		displayLineInTermbox(fmt.Sprintf("%s%s%s", previousIndent, indent, f.Name()), f.IsDir())
-		if f.IsDir() && folderLevelMinned > 1 {
-			s := []string{path, f.Name()}
-			nextPath := strings.Join(s, "/")
-			s[0] = previousIndent
-			s[1] = nextIndent
-			displayDir(nextPath, fmt.Sprintf("%s%s", previousIndent, nextIndent))
-		}
+
+		displayLineInTermbox(f.Name(), f.IsDir(), first)
+
+		files = append(files, File{f.IsDir(), f.Name()})
+
+		i += 1
 	}
-	termbox.Flush()
 }
 
-func displayLineInTermbox(name string, isDir bool) {
-	// TODO transform name string in runes ?
+func displayLineInTermbox(name string, isDir bool, selected bool) {
 	x := 0
 	for _, r := range name {
-		if isDir {
-			termbox.SetCell(x, termboxLine, r, termbox.ColorGreen, termbox.ColorDefault)
-		} else {
-			termbox.SetCell(x, termboxLine, r, termbox.ColorDefault, termbox.ColorDefault)
-		}
+		draw(x, termboxLine, r, isDir, selected)
 
 		x += 1
 	}
@@ -107,4 +118,97 @@ func getRelativeLevelPath(folderPath string) (levelPath int) {
 	levelPath = strings.Count(folderPath, "/")
 
 	return
+}
+
+func draw(x int, y int, r rune, isDir bool, selected bool) {
+	bg := termbox.ColorDefault
+	if selected {
+		bg = termbox.ColorMagenta
+	}
+
+	fg := termbox.ColorDefault
+	if isDir {
+		fg = termbox.ColorGreen
+	}
+	
+	termbox.SetCell(x, y, r, fg, bg)
+}
+
+func changeSelect(position string) {
+	if "down" == position && currentLine == len(files) - 1 {
+		return
+	}
+
+	if "up" == position && currentLine == 0 {
+		return
+	}
+
+	x := 0
+	for _, r := range files[currentLine].name {
+		draw(x, currentLine + 1, r, files[currentLine].isDir, false)
+
+		x += 1
+	}
+
+	if "up" == position {
+		currentLine -= 1
+	} else {
+		currentLine += 1
+	}
+
+	x = 0
+	for _, r := range files[currentLine].name {
+		draw(x, currentLine + 1, r, files[currentLine].isDir, true)
+
+		x += 1
+	}
+
+	termbox.Flush()
+}
+
+func enterDir() {
+	if !files[currentLine].isDir {
+		return;
+	}
+
+	termboxLine = 1
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+
+	s := []string{currentPath, files[currentLine].name}
+	currentPath = strings.Join(s, "/")
+
+	files = make([]File, 0)
+
+	displayFirstLine(currentPath)
+	displayDir(currentPath)
+
+	termbox.Flush()
+
+	currentLine = 0
+}
+
+func leaveDir() {
+	if currentPath == rootPath {
+		return
+	}
+
+	termboxLine = 1
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+
+	splitPath := strings.Split(currentPath, "/")
+	max := len(splitPath) - 1
+	currentPath = strings.Join(splitPath[0:max], "/")
+
+	files = make([]File, 0)
+
+	displayFirstLine(currentPath)
+	displayDir(currentPath)
+
+	termbox.Flush()
+
+	currentLine = 0
 }
